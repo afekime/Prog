5a975a16f779e79c26301893a50fc9a45dc5eb90
Pavel <afekime1@gmail.com>
2026-02-19 11:30:26 +0700

    Лабораторная работа 1

--- Lab1.py
+++ Lab1.py
@@ -21,6 +21,34 @@ def build_obj(line: str):
 
     return Phone(name, release_date, ram_capability)
 
+stroka="1+223456*+2+1*3345678+"
+
+def arifm(s):
+    best_start = 0
+    best_end = 0
+    for startSub in range(len(s)):
+        if s[startSub] < '0' or s[startSub] > '9':
+            continue
+
+        endSub = startSub+1
+        while endSub < len(s)-1:
+            if (s[endSub] < '0' or s[endSub] > '9') and (s[endSub+1] < '0' or s[endSub+1] > '9'):
+                break
+            endSub+=1
+
+        cur_start = startSub
+        cur_end = endSub
+
+        if cur_end - cur_start > best_end - best_start:
+            best_start = cur_start
+            best_end = cur_end
+
+        best_s = s[best_start:best_end]
+    return best_s
+
+print(arifm(stroka))
+
+
 line = input()
 obj = build_obj(line)
 print(obj)
